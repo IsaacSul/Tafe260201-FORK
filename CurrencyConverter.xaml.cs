46cf99daa2363757502c9c9441962728bce39a2a
Jaxon Mossop <jaxon.mossop@student.tafesa.edu.au>
2026-08-31 15:30:57 +0930

    Added Currency Converter UI

--- CurrencyConverter.xaml.cs
+++ CurrencyConverter.xaml.cs
@@ -20,16 +20,11 @@ namespace Calculator
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Menu : Page
+    public sealed partial class CurrencyConverter : Page
     {
-        public Menu()
+        public CurrencyConverter()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(CurrencyConverter));
         }
     }
 }
